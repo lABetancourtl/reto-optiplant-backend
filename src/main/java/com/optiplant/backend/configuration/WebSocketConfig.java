427d4f3179c7287f9fef430eda312ccb04f0e21b
Anderson Betancourt Arenas <111938729+lABetancourtl@users.noreply.github.com>
2026-03-19 16:17:04 -0500

    WebSocketConfig
    Error corregido en WebSocketConfig

--- src/main/java/com/optiplant/backend/configuration/WebSocketConfig.java
+++ src/main/java/com/optiplant/backend/configuration/WebSocketConfig.java
@@ -1,9 +1,11 @@
 package com.optiplant.backend.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import com.optiplant.backend.configuration.security.StompAuthChannelInterceptor;
 
@@ -14,6 +16,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final StompAuthChannelInterceptor stompAuthChannelInterceptor;
 
     public WebSocketConfig(StompAuthChannelInterceptor stompAuthChannelInterceptor) {
+        this.stompAuthChannelInterceptor = stompAuthChannelInterceptor;
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
         registry.setUserDestinationPrefix("/user");
     }
 
@@ -24,9 +32,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.enableSimpleBroker("/topic");
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*");
-
     }
 }
